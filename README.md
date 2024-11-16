b70f050bd5df57490617fc28d6ca04fe0cf71562
Mochytk <116850132+Mochytk@users.noreply.github.com>
2024-11-16 13:18:13 -0300

    Añadir archivos

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tarea_2-3Algoco
+Aquí se encontrarán los códigos de los dos algoritmos pedidos en el informe.
---
+++ bruteforce.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fastio(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.setf(ios::fixed);
+    cout.precision(4);
+}
+
+typedef long long ll;
+
+int main(){
+    fastio();
+    return 0;
+}
